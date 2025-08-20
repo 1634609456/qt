ac4b5c21b5522f4957f7a8e226750160cd63cb00
wx <1634609456@qq.com>
2025-08-20 14:42:58 +0800

    使用定时器定时查询主轴实际速度

--- ui/home_page/homepage.cpp
+++ ui/home_page/homepage.cpp
@@ -1,21 +1,42 @@
 #include "homepage.h"
 #include "ui_homepage.h"
 
+#include <QDebug>
+#include <QTimer>
+#include <cstdlib>
+#include <qtimer.h>
+#include <winsock.h>
+
 #include "../../src/util/shm_manager.hpp"
 
-
 HomePage::HomePage(QWidget *parent)
-    : QWidget(parent)
-    , ui(new Ui::HomePage)
-    , buffer(nullptr) // 初始化环形缓冲区
+    : QWidget(parent), ui(new Ui::HomePage), buffer(nullptr) // 初始化环形缓冲区
 {
-    ui->setupUi(this);
+  ui->setupUi(this);
 
-    connect(&ShmManager::get_instance(), &ShmManager::loaded, [this](bool success) {
+
+          // 主轴速度读取
+          auto timer = new QTimer(this);
+          connect(timer, &QTimer::timeout, this, [this]() {
+            // // 模拟数据更新
+            // auto data = rand() % 100;
+
+            auto data = ShmManager::get_instance()
+                            .get_data()
+                            ->feedback.motor_fdb[MAIN_SPINDLE]
+                            .running_speed;
+            ui->label_5->setText(QString::number(data));
+          });
+
+  connect(
+      &ShmManager::get_instance(), &ShmManager::loaded, [this, timer](bool success) {
+
         if (success) {
-            buffer.set_buffer(&ShmManager::get_instance().get_data()->buffer_P);
+          buffer.set_buffer(&ShmManager::get_instance().get_data()->buffer_P);
+
+          timer->start(200);
         }
-    });
+      });
 }
 
 HomePage::~HomePage()
@@ -33,5 +54,14 @@ void HomePage::on_pushButton_14_clicked()
     });
 
     qDebug() << "运行按钮";
+}
+
+
+void HomePage::on_lineEdit_editingFinished()
+{
+
+    auto test = ui->lineEdit->text().toInt();
+    qDebug() << "text中的数据" << test;
+    
 }
 
--- ui/home_page/homepage.h
+++ ui/home_page/homepage.h
@@ -22,6 +22,7 @@ public:
 
 private slots:
     void on_pushButton_14_clicked();
+    void on_lineEdit_editingFinished();
 
 private:
     Ui::HomePage *ui;
